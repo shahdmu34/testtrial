676074c164ebc3d287663fd4b6f913ba0acecb79
Shahd Mustafa <shahdmu@umich.edu>
2022-04-18 14:38:59 -0400

    selection sort commit

---
+++ Selectionsort.java
@@ -0,0 +1,18 @@
+public class SelectionSort {
+private int temp;
+ /** Creates a new instance of SelectionSort */
+public SelectionSort() {
+     }
+
+
+     public int[] basicSelectionSort(int[] x) {
+for (int i = 0; i < x.length; ++i) {
+for (int j= i+1; j < x.length; ++j) {
+if (x[i] > x[j]) {                      temp = x[i];
+x[i] = x[j];                      x[j] = temp;
+                 }
+             } // end of inner for loop
+} // end of outer for loop
+return x;
+     }
+}
